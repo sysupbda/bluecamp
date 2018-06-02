51c34a7de7295df04f29288bd4c9a31e5ef809ac
Ben Dadsetan <behrang.dadsetan@p2l.tv>
2018-06-02 16:48:48 +0200

    feat(db): Initial table structure and migration scripts

--- server/migrations/20180602155852_init.ts
+++ server/migrations/20180602155852_init.ts
@@ -1,9 +1,32 @@
 import * as Knex from 'knex';
 
 exports.up = async function(knex: Knex): Promise<void> {
+  await knex.schema.createTable('audience', table => {
+    table.increments('id').primary('audience_pk');
+    table.string('name', 255).unique();
+  });
 
+  await knex.schema.createTable('location', table => {
+    table.increments('id').primary('location_pk');
+    table.string('name', 255).unique();
+    table.decimal('lat', 15, 7);
+    table.decimal('long', 15, 7);
+  });
+
+  await knex.schema.createTable('schedule', table => {
+    table.increments('id').primary();
+    table.string('name');
+    table.dateTime('start');
+    table.dateTime('end');
+    table.integer('audience_id');
+    table.integer('location_id');
+    table.foreign('audience_id').references('audience.id');
+    table.foreign('location_id').references('location.id');
+  });
 };
 
 exports.down = async function(knex: Knex): Promise<void> {
-
+  await knex.schema.dropTable('schedule');
+  await knex.schema.dropTable('location');
+  await knex.schema.dropTable('audience');
 };
